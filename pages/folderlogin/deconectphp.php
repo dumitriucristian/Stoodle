d856afe43de7a19162f30a92407194b2648f8519
Robertbombardierul <robertplaiasu03@gmail.com>
2020-05-02 04:16:54 +0300

    mic update

--- pages/folderlogin/deconectphp.php
+++ pages/folderlogin/deconectphp.php
@@ -1,7 +1,7 @@
 <?php
+session_start();
   if(isset($_SESSION['mailUser']))
   {
-  session_start();
   session_unset();
   session_destroy();
   header("Location: ../../indexpp.php?logout=succes");
