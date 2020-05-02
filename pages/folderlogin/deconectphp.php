9481706958140ea071ce699bea4a30353cd0db2a
Robertbombardierul <robertplaiasu03@gmail.com>
2020-05-02 03:36:47 +0300

    deconect

--- pages/folderlogin/deconectphp.php
+++ pages/folderlogin/deconectphp.php
@@ -1,5 +1,17 @@
 <?php
+  if(isset($_SESSION['mailUser']))
+  {
   session_start();
   session_unset();
   session_destroy();
   header("Location: ../../indexpp.php?logout=succes");
+  exit();
+}
+  if (isset($_SESSION['mailGmail']) {
+    require_once 'google-config.php';
+    unset($_SESSION['acces_token']);
+    $client->revokeToken();
+    session_destroy();
+    header("Location: ../../indexpp.php?logout=succes");
+    exit();
+  }
