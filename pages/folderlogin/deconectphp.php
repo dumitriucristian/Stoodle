6bd525c51f7c31ea83d8c66a648f73d262b38091
Robertbombardierul <robertplaiasu03@gmail.com>
2020-05-03 13:11:20 +0300

    update index

--- pages/folderlogin/deconectphp.php
+++ pages/folderlogin/deconectphp.php
@@ -1,22 +1,18 @@
 <?php
 session_start();
-  if(isset($_SESSION['mailUser']))
-  {
+function deconect(){
   session_unset();
   session_destroy();
-  setcookie("select", $selector,-60*60*24*30,"/");
-  setcookie("validator",$token,-60*60*24*30,"/");
-  header("Location: ../../indexpp.php?logout=succes");
+  setcookie("select", " ",-60*60*24*30,"/");
+  setcookie("validator","",-60*60*24*30,"/");
+  header("Location: ../../index.php?logout=succes");
   exit();
-
 }
+  if(isset($_SESSION['mailUser']))
+  deconect();
   if (isset($_SESSION['mailGmail'])) {
     require_once 'google-config.php';
     unset($_SESSION['acces_token']);
     $client->revokeToken();
-    session_destroy();
-    setcookie("select", $selector,-60*60*24*30,"/");
-    setcookie("validator",$token,-60*60*24*30,"/");
-    header("Location: ../../indexpp.php?logout=succes");
-    exit();
+    deconect();
   }
