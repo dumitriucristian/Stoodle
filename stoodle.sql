20c5afa5432e00ec060623491ffb6c50a1d5bfc8
GrigoAleex <grigorescu.aleex@gmail.com>
2020-05-03 20:02:17 +0300

    Stoodle

--- stoodle.sql
+++ stoodle.sql
@@ -2,10 +2,10 @@
 -- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 03, 2020 at 02:13 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.3
+-- Gazdă: 127.0.0.1
+-- Timp de generare: mai 03, 2020 la 07:01 PM
+-- Versiune server: 10.4.11-MariaDB
+-- Versiune PHP: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `stoodle`
+-- Bază de date: `stoodle`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `auth`
+-- Structură tabel pentru tabel `auth`
 --
 
 CREATE TABLE `auth` (
@@ -37,12 +37,18 @@ CREATE TABLE `auth` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `auth`
+-- Eliminarea datelor din tabel `auth`
+--
+
+INSERT INTO `auth` (`id`, `validator`, `selector`, `userid`, `data`) VALUES
+(1, '$2y$10$rbcScoyEbNBEowPr28S3DuBYgdMrt/ClQheK0nVvWRLrvWgBFwb9m', '409cd1463c8cd52d35c8242742fd84b434bfd3762ed1371b', 0, 1590944748),
+(2, '$2y$10$NQxxNvU3ulkF7TT1XqdUr.931btky4ezf0vArnh.IGoposrajjwv2', '06c939c57218bf2aedf542c95a5ba3f4776b000c1922873d', 6, 1590968644),
+(5, '$2y$10$qZFyTeHPN2bVXV9mnbH8WejhzmqQHd27OHDXQpr9R7BmKLDiH3J4G', 'af340feaf43d1adc68d29e905712ce81bdf348b0bbd14d64', 1, 1591113640);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `facultati`
+-- Structură tabel pentru tabel `facultati`
 --
 
 CREATE TABLE `facultati` (
@@ -66,25 +72,23 @@ CREATE TABLE `facultati` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `facultati`
+-- Eliminarea datelor din tabel `facultati`
 --
 
 INSERT INTO `facultati` (`Indexf`, `Numef`, `Judet`, `Examenadmi`, `Universitatea`, `Profil`, `Poza`, `job`, `pasiune_facultati`, `materie1`, `materie2`, `materie3`, `carti`, `sociabil`, `sport`, `stres`, `link_facultate`) VALUES
 (1, 'Facultatea de Teologie Ortodoxa UBB', 'Cluj', 1, 'Universitatea Babes-Bolyai', 'Teologic', 'teologiccluj.jpg', 1, 'Religie', 'Religie', 'Istorie', 'Muzica', 'Religie', 1, '0', 0, 'http://ot.ubbcluj.ro/'),
-(2, 'Facultatea de Automatica si Calculatoare                                                                                                                  ', 'Cluj', 1, 'Universitatea Tehnica Cluj', 'Mate-info', 'ACcluj.jpg', 0, 'Programare/Calculatoare', 'Informatica', 'Matematica', 'Fizica', 'Tehnica si tehnologie', 0, '0', 1, 'https://acs.pub.ro/'),
+(2, 'Facultatea de Automatica si Calculatoare                                                                                                                  ', 'Bucuresti', 1, 'Universitatea Politehnica din Bucuresti', 'Mate-info', 'ACcluj.jpg', 0, 'Programare/Calculatoare', 'Informatica', 'Matematica', 'Fizica', 'Tehnica si tehnologie', 0, '0', 1, 'https://acs.pub.ro/'),
 (3, 'Facultatea de Psihologie si Stiinte ale Educatiei UBB', 'Cluj', 1, 'Universitatea Babes-Bolyai', 'Filologie', 'Pshihologiecluj.jpg', 1, 'Psihologie', 'Psihologie', 'Limba si literatura romana', 'Sociologie', 'Psihologie', 1, '0', 0, 'https://psiedu.ubbcluj.ro/'),
 (4, 'Facultatea de Biologie si Geologie UBB', 'Cluj', 1, 'Universitatea Babes-Bolyai', 'Stiinte ale naturii', 'biologiecluj.jpg', 1, 'Biologie', 'Biologie', 'Chimie', 'Fizica', 'Enciclopedii', 0, '0', 0, 'http://bioge.ubbcluj.ro/'),
 (5, 'Facultatea de Chimie si Inginerie Chimica', 'Cluj', 1, 'Universitatea Babes-Bolyai', 'Stiinte ale naturii', 'chimiecluj.jpg', 0, 'Chimie', 'Chimmie', 'Fizica', 'Matematica', 'Stiinte exacte', 0, '0', 1, 'http://chem.ubbcluj.ro/'),
-(6, 'Faculatatea de Istorie UniBuc ', 'Bucuresti', 1, 'UniBuc', 'Filologie', 'default.jpg', 1, 'Istorie', 'Istorie', 'Religie', 'Geografie', 'Istorie', 0, '0', 0, 'https://istorie.unibuc.ro/'),
-(8, 'Facultatea de Jurnalism si Stiintele Comunicarii UniBuc', 'Bucuresti', 1, 'UniBuc', 'Stiinte-sociale', 'default.jpg', 1, 'Jurnalism', 'Limba si literatura romana', 'Educatie civica', 'Engleza', 'Stiinte sociale,politica', 1, '1', 0, 'http://www.fjsc.unibuc.ro/'),
-(9, 'Facultatea de Limbi si Literaturi Straine UniBuc ', 'Bucuresti', 1, 'Unibuc', 'Filologie', 'default.jpg', 1, 'Limbi straine', 'Engleza', 'Franceza', 'Germana', 'Limbi straine', 1, '0', 0, 'http://lls.unibuc.ro/'),
-(10, 'Facultatea de litere UniBuc', 'Bucuresti', 0, 'UniBuc', 'Filologie', 'default.jpg', 1, 'Literatura', 'Limba si literatura romana', 'Engleza', 'Franceza', 'Poezie/Literatura', 1, '0', 1, 'https://unibuc.ro/studii/facultati/facultatea-de-litere/'),
-(11, 'Facultatea de Matematica si Informatica UniBuc', 'Bucuresti', 1, 'UniBuc', 'Mate-info', 'default.jpg', 0, 'Matematica', 'Matematica', 'Informatica', 'TIC', 'Stiinte exacte', 0, '0', 1, 'http://fmi.unibuc.ro/ro/');
+(7, 'Facultatea de Medicină', 'Iasi', 1, 'Universitatea de Medicină și Farmacie \"Grigore T. Popa\", Iași', 'Stiinte ale naturii', 'default.jpg', 0, 'Medicina', 'Matematica', 'Chimie', 'Biologie', 'Medicina', 1, '1', 1, 'http://www.umfiasi.ro/'),
+(8, 'Facultatea de Fizică', 'Bucuresti', 1, 'Universitatea din București', 'Mate-info', 'default.jpg', 0, 'Fizica', 'Matematica', 'Fizica', 'Chimie', 'Stiinte exacte', 0, '0', 0, 'http://www.fizica.unibuc.ro/Fizica/Main.php'),
+(9, 'Facultatea de Istorie', 'Bucuresti', 1, 'Universitatea din București', 'Filologie', 'default.jpg', 1, 'Istorie', 'Istorie', 'Educatie civica', 'Geografie', 'Istorie', 0, '0', 0, 'https://istorie.unibuc.ro/admitere2019/admitere-master/');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `favorite`
+-- Structură tabel pentru tabel `favorite`
 --
 
 CREATE TABLE `favorite` (
@@ -95,13 +99,16 @@ CREATE TABLE `favorite` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `favorite`
+-- Eliminarea datelor din tabel `favorite`
 --
+
+INSERT INTO `favorite` (`id`, `idUser`, `Indexf`, `tip`) VALUES
+(1, 1, 3, 'normal');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `resetare`
+-- Structură tabel pentru tabel `resetare`
 --
 
 CREATE TABLE `resetare` (
@@ -115,7 +122,7 @@ CREATE TABLE `resetare` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Structură tabel pentru tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -140,10 +147,17 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Eliminarea datelor din tabel `users`
+--
+
+INSERT INTO `users` (`idUser`, `Nume`, `Prenume`, `mailUser`, `pwdUsers`, `Profil`, `Domeniu`, `domeniu_intensitate`, `job`, `materie1`, `materie2`, `materie3`, `carti`, `sociabil`, `sport`, `stres`, `Judet`, `PozaUser`) VALUES
+(1, 'Grigorescu', 'Alexandru', 'grigorescu.aleex@gmail.com', '$2y$10$I6urXCEv4Klu7uMza1J9vejKMVVSJcoqZdFXTPRaFLfuTANTbZNZm', 'Mate-info', 'Programare/Calculatoare', 5, 0, 'Matematica', 'Fizica', 'TIC', 'Psihologie', 0, 'Da', 0, 'Prahova', 'UserDefault.jpg'),
+(6, 'Nume', 'Robert', 'robertplaiasu03@gmail.com', '$2y$10$uJ2AnpXLNXq3Uenquyf3DurigB7ISuaGjWsaxdIlGrvN3OIoXAKpG', 'Filologie', 'Drept', 4, 0, 'Muzica', 'Economie', 'Educatie civica', 'Psihologie', 0, 'Nu', 0, 'Dambovita', 'UserDefault.jpg');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users_gmail`
+-- Structură tabel pentru tabel `users_gmail`
 --
 
 CREATE TABLE `users_gmail` (
@@ -166,12 +180,17 @@ CREATE TABLE `users_gmail` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Eliminarea datelor din tabel `users_gmail`
+--
 
+INSERT INTO `users_gmail` (`idGmail`, `numeGmail`, `prenumeGmail`, `mailGmail`, `Profil`, `Domeniu`, `domeniu_intensitate`, `job`, `materie1`, `materie2`, `materie3`, `carti`, `sociabil`, `sport`, `stres`, `Judet`) VALUES
+(1, 'Grigorescu', 'Alexandru', 'grigorescu.aleex@gmail.com', 'Teologic', 'Medicina', 1, 0, 'Istorie', 'Geografie', 'Educatie civica', 'Culinare', 0, 0, 0, 'Alba'),
+(2, '', 'Grigo', 'xalexmcx@gmail.com', '', '', 0, 0, '', '', '', '', 0, 0, 0, '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users_verificare`
+-- Structură tabel pentru tabel `users_verificare`
 --
 
 CREATE TABLE `users_verificare` (
@@ -187,96 +206,100 @@ CREATE TABLE `users_verificare` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users_verificare`
+-- Eliminarea datelor din tabel `users_verificare`
 --
+
+INSERT INTO `users_verificare` (`idVerificare`, `numeVerificare`, `prenumeVerificare`, `mailVerificare`, `parolaVerificare`, `selectVerificare`, `tokenVerificare`, `expireVerificare`, `verificare`) VALUES
+(1, 'Nume', 'Robert', 'robertplaiasu03@gmail.com', '$2y$10$uJ2AnpXLNXq3Uenquyf3DurigB7ISuaGjWsaxdIlGrvN3OIoXAKpG', '3ff6a069d63330bfe06cacba', '$2y$10$tZs06egu/bbkBQ2dmFvhT.fSDA9RWBm6wcafRTxqPPhosZrYSI8PG', 1586774491, NULL);
+
 --
--- Indexes for dumped tables
+-- Indexuri pentru tabele eliminate
 --
 
 --
--- Indexes for table `auth`
+-- Indexuri pentru tabele `auth`
 --
 ALTER TABLE `auth`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `facultati`
+-- Indexuri pentru tabele `facultati`
 --
 ALTER TABLE `facultati`
   ADD PRIMARY KEY (`Indexf`);
 
 --
--- Indexes for table `favorite`
+-- Indexuri pentru tabele `favorite`
 --
 ALTER TABLE `favorite`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `resetare`
+-- Indexuri pentru tabele `resetare`
 --
 ALTER TABLE `resetare`
   ADD PRIMARY KEY (`idReset`);
 
 --
--- Indexes for table `users`
+-- Indexuri pentru tabele `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`idUser`);
 
 --
--- Indexes for table `users_gmail`
+-- Indexuri pentru tabele `users_gmail`
 --
 ALTER TABLE `users_gmail`
   ADD PRIMARY KEY (`idGmail`);
 
 --
--- Indexes for table `users_verificare`
+-- Indexuri pentru tabele `users_verificare`
 --
 ALTER TABLE `users_verificare`
   ADD PRIMARY KEY (`idVerificare`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pentru tabele eliminate
 --
 
 --
--- AUTO_INCREMENT for table `auth`
+-- AUTO_INCREMENT pentru tabele `auth`
 --
 ALTER TABLE `auth`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `facultati`
+-- AUTO_INCREMENT pentru tabele `facultati`
 --
 ALTER TABLE `facultati`
-  MODIFY `Indexf` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `Indexf` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT for table `favorite`
+-- AUTO_INCREMENT pentru tabele `favorite`
 --
 ALTER TABLE `favorite`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
--- AUTO_INCREMENT for table `resetare`
+-- AUTO_INCREMENT pentru tabele `resetare`
 --
 ALTER TABLE `resetare`
   MODIFY `idReset` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT pentru tabele `users`
 --
 ALTER TABLE `users`
   MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `users_gmail`
+-- AUTO_INCREMENT pentru tabele `users_gmail`
 --
 ALTER TABLE `users_gmail`
-  MODIFY `idGmail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idGmail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `users_verificare`
+-- AUTO_INCREMENT pentru tabele `users_verificare`
 --
 ALTER TABLE `users_verificare`
   MODIFY `idVerificare` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
